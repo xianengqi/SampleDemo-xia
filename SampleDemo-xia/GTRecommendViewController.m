486148a48d4f47fe3e6a5f20d61340017fc5edc9
xianengqi <xianengqi@gmail.com>
2020-08-19 23:01:56 +0800

    feat: 编写回调函数

--- SampleDemo-xia/GTRecommendViewController.m
+++ SampleDemo-xia/GTRecommendViewController.m
@@ -8,7 +8,8 @@
 
 #import "GTRecommendViewController.h"
 
-@interface GTRecommendViewController ()
+// 需要在这里声明一下使用的delaget
+@interface GTRecommendViewController ()<UIScrollViewDelegate>
 
 @end
 
@@ -35,6 +36,9 @@
     // `self.view.bounds.size.width * 5` 表示，有五个屏幕的大小
     scrollView.contentSize = CGSizeMake(self.view.bounds.size.width * 5, self.view.bounds.size.height);
     
+    // 这是拖拽要设置的
+    scrollView.delegate = self;
+    
     NSArray *colorArray = @[[UIColor redColor], [UIColor blueColor], [UIColor yellowColor], [UIColor lightGrayColor], [UIColor grayColor]];
     
     for (int i = 0; i < 5; i++) {
@@ -48,6 +52,46 @@
     scrollView.pagingEnabled = YES;
     [self.view addSubview:scrollView];
 }
+// `scrollViewDidScroll`监听页面滚动,以及根据Offset做业务逻辑
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    // 打log,和javaScript里一样，可以打log，在javascript里使用console.log,在Object-c这里是用NSLog
+  //  NSLog(@"scrollViewDidScroll - %@", @(scrollView.contentOffset.x)); // 获取当前标志scrollView在哪,获得水平方向的contentOffset
+}
+
+// 纵横的先暂时不使用
+// - (void)scrollViewDidZoom:(UIScrollView *)scrollView API_AVAILABLE(ios(3.2)); // any zoom scale changes
+
+// 拖拽的函数, 开始拖拽，中断一些业务逻辑，如视频/gif播放
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    NSLog(@"BeginDragging");
+}
+
+// 纵横的先暂时不使用
+//- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset API_AVAILABLE(ios(5.0)){
+//
+//}
+
+// 拖拽的函数 结束拖拽， 开始拖拽，中断一些业务逻辑，如视频/gif播放
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+    NSLog(@"EndDragging");
+}
+
+// 拖拽的减速开始 ，页面停止时开始逻辑，如视频自动播放
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView{
+}
+
+// 拖拽的减速结束，页面停止时开始逻辑，如视频自动播放
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+}
+
 
 
 @end
+
+//总结：
+//UIScrollView
+//1. 是 UICollectionView 和 UITableView 的父类
+//2. frame 决定视图在屏幕中展示的大小
+//3. contentSize 视图内部内容可以滚动的区域
+//4. contentOffset 滚动区域左上角与视图左上角的距离
+//5. pagingEnabled 可以实现翻页效果
