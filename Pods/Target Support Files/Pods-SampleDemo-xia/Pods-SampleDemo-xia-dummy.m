100d4289ba4eb0524d298822e695d8c5cf712872
xianengqi <xianengqi@gmail.com>
2020-08-26 01:10:42 +0800

    feat: 使用第三方库封装网络请求

---
+++ Pods/Target Support Files/Pods-SampleDemo-xia/Pods-SampleDemo-xia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleDemo_xia : NSObject
+@end
+@implementation PodsDummy_Pods_SampleDemo_xia
+@end
--- SampleDemo-xia/loader/GTListLoader.m
+++ SampleDemo-xia/loader/GTListLoader.m
@@ -7,17 +7,38 @@
 //
 
 #import "GTListLoader.h"
+#import <AFNetworking.h>
 
 @implementation GTListLoader
 
 - (void)loadListData{
    
-    NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
-    NSURL *listURL = [NSURL URLWithString:urlString];
+
+    // 使用第三方库封装的网络请求
+    [[AFHTTPSessionManager manager] GET:@"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e" parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"");
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"");
+    }];
     
-    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+//    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
     
-    NSLog(@"");
+    // 自己用系统内部的方法封装的网络请求
+//    NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
+//    NSURL *listURL = [NSURL URLWithString:urlString];
+//
+//    // 将request封装成task进行操作
+//    NSURLSession * session = [NSURLSession sharedSession];
+//    // 封装最基础的dataTask
+//    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//        NSLog(@"");
+//    }];
+//
+//    [dataTask resume];
+//
+//    NSLog(@"");
 }
 
 @end
